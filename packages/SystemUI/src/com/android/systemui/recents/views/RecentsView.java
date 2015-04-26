180e952e711abd97f6732572f26fe797f377e66c
chickenfactory <micah.chickenfactory@gmail.com>
2015-04-25 20:17:31 -1000

    decrease value for FAB button a tad bit

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -378,11 +378,11 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             } else if (mSearchBar == null && enableMemDisplay) {
             	params.topMargin = mContext.getResources().
                     getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height)
-                        + 125;
+                        + 100;
             } else if (mSearchBar != null && enableMemDisplay) {
             	params.topMargin = mContext.getResources().
                     getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height)
-                        + searchBarSpaceBounds.height() + 125;
+                        + searchBarSpaceBounds.height() + 100;
             } else {
                 params.topMargin = mContext.getResources().
                     getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height)
