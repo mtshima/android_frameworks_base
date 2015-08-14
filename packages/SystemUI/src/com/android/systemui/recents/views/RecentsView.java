85714abe35695ea2c51d8ccd7d551184c95f08ce
chickenfactory <micah.chickenfactory@gmail.com>
2015-08-13 20:51:09 -1000

    lower FAB button more commit: https://github.com/mtshima/android_frameworks_base/commit/60df04d2d0aeedf3a25107aba73775018f97b8d3

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -400,11 +400,11 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             } else if (mSearchBar == null && enableMemDisplay) {
             	params.topMargin = mContext.getResources().
                     getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height)
-                        + 125;
+                        + 130;
             } else if (mSearchBar != null && enableMemDisplay) {
             	params.topMargin = mContext.getResources().
                     getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height)
-                        + searchBarSpaceBounds.height() + 125;
+                        + searchBarSpaceBounds.height() + 130;
             } else {
                 params.topMargin = mContext.getResources().
                     getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height)
