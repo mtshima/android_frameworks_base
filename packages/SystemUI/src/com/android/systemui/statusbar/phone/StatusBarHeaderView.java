dcb5c9304bd3cce1c261052b738b31ab6e8367f6
chickenfactory <micah.chickenfactory@gmail.com>
2015-08-22 10:59:53 -1000

    change from false to true for dismissKeyguard commits: https://github.com/mtshima/android_frameworks_base/commit/658fa880d785d69fd46c6008ea34a97bd9601a59 https://github.com/mtshima/android_frameworks_base/commit/7595cc2522bd89d45f9998f08d4b7705bd9f3fa1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -738,14 +738,14 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.setClassName("com.android.settings",
             "com.android.settings.Settings$HeadsUpSettingsActivity");
-        mActivityStarter.startActivity(intent, true /* dismissShade */, false /* dismissKeyguard */);
+        mActivityStarter.startActivity(intent, true /* dismissShade */, true /* dismissKeyguard */);
     }
 
     private void startTaskManagerLongClickActivity() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.setClassName("com.android.settings",
             "com.android.settings.Settings$RunningServicesActivity");
-        mActivityStarter.startActivity(intent, true /* dismissShade */, false /* dismissKeyguard */);
+        mActivityStarter.startActivity(intent, true /* dismissShade */, true /* dismissKeyguard */);
     }
 
     public void setQSPanel(QSPanel qsp) {
