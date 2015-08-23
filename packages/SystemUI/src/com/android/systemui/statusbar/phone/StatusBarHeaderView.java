99e27c0408b3bcbb9643d1b3e42de355dd572bcc
chickenfactory <micah.chickenfactory@gmail.com>
2015-08-23 01:58:47 -1000

    change color of status bar header

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -289,7 +289,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private void updateBackgroundColor() {
         ContentResolver resolver = mContext.getContentResolver();
         int backgroundColor = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_EXPANDED_HEADER_BG_COLOR, 0xff384248);
+                Settings.System.STATUS_BAR_EXPANDED_HEADER_BG_COLOR, 0xee263238);
         getBackground().setColorFilter(backgroundColor, Mode.MULTIPLY);
     }
     
