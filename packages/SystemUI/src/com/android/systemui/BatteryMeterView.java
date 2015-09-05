da2e481c2d8bfa64ff4d03ddaf48999af0965478
chickenfactory <micah.chickenfactory@gmail.com>
2015-09-04 15:57:44 -1000

    Revert "SystemUI: Fix blinking when reported battery level is 0"
    This reverts commit a435ad6808993aaacd174fe851708e4bb2a2d4fa.

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -887,11 +887,8 @@ public class BatteryMeterView extends View implements DemoMode,
 
             // draw thin gray ring first
             canvas.drawArc(drawRect, 270, 360, false, mBackPaint);
-
-            if (level != 0) {
-                // draw colored arc representing charge level
-                canvas.drawArc(drawRect, 270 + mAnimOffset, 3.6f * level, false, paint);
-            }
+            // draw colored arc representing charge level
+            canvas.drawArc(drawRect, 270 + mAnimOffset, 3.6f * level, false, paint);
             // if chosen by options, draw percentage text in the middle
             // always skip percentage when 100, so layout doesnt break
             if (unknownStatus) {
