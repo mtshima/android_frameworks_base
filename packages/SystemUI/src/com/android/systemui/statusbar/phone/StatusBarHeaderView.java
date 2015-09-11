0973e6893d0db0732449efff2f2620ef40dc499b
chickenfactory <micah.chickenfactory@gmail.com>
2015-09-10 17:17:18 -1000

    Revert "add more boolean statements"
    This reverts commit 658fa880d785d69fd46c6008ea34a97bd9601a59.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -694,19 +694,19 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         Intent intent = new Intent(Intent.ACTION_MAIN);
 	intent.setClassName("com.android.settings",
             "com.android.settings.Settings$QSTilesSettingsActivity");
-        mActivityStarter.startActivity(intent, true /* dismissShade */, true /* dismissKeyguard */);
+        mActivityStarter.startActivity(intent, true /* dismissShade */);
     }
 
     private void startBatteryActivity() {
         mActivityStarter.startActivity(new Intent(Intent.ACTION_POWER_USAGE_SUMMARY),
-                true /* dismissShade */, true /* dismissKeyguard */, true /* dismissKeyguard */);
+                true /* dismissShade */, true /* dismissKeyguard */);
     }
 
     private void startBatteryLongClickActivity() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.setClassName("com.android.settings",
             "com.android.settings.Settings$BatterySaverSettingsActivity");
-        mActivityStarter.startActivity(intent, true /* dismissShade */. true /* dismissKeyguard */);
+        mActivityStarter.startActivity(intent, true /* dismissShade */);
     }
 
     private void startClockActivity() {
